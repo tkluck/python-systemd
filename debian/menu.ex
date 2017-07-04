b182fd85156f1b3738b94db09d5b59aa123d2ef0
Timo Kluck <tkluck@infty.nl>
2017-07-04 10:05:18 +0200

    Remove example files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(python-systemd-cython):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="python-systemd-cython" command="/usr/bin/python-systemd-cython"
--- debian/python-systemd-cython.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the python-systemd-cython package
-#
-0 4	* * *	root	[ -x /usr/bin/python-systemd-cython_maintenance ] && /usr/bin/python-systemd-cython_maintenance
