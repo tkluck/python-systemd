f239c51d18e345081b5d66f5e2a06f8dba42697f
Timo Kluck <tkluck@infty.nl>
2017-07-04 10:00:19 +0200

    Commit files generated by "dh_make -p python-systemd-cython_0.9.15 --createorig"

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(python-systemd-cython):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="python-systemd-cython" command="/usr/bin/python-systemd-cython"
---
+++ debian/python-systemd-cython.default.ex
@@ -0,0 +1,10 @@
+# Defaults for python-systemd-cython initscript
+# sourced by /etc/init.d/python-systemd-cython
+# installed at /etc/default/python-systemd-cython by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
